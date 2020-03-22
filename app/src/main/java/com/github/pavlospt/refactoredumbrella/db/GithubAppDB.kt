ea70144237a3233f1919717da1e5462fceacb06a
pavlospt <p.tournaris@gmail.com>
2020-03-23 00:30:20 +0200

    Disable schema export in Room DB

--- app/src/main/java/com/github/pavlospt/refactoredumbrella/db/GithubAppDB.kt
+++ app/src/main/java/com/github/pavlospt/refactoredumbrella/db/GithubAppDB.kt
@@ -3,7 +3,7 @@ package com.github.pavlospt.refactoredumbrella.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [GithubRepoEntity::class], version = 1)
+@Database(entities = [GithubRepoEntity::class], exportSchema = false, version = 1)
 abstract class GithubAppDB : RoomDatabase() {
     companion object {
         const val DB_NAME = "github_app.db"
