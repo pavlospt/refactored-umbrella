c8522b53475cbff0f409628611fe99aeb89d4cf8
pavlospt <p.tournaris@gmail.com>
2020-05-06 10:27:55 +0300

    Remove not needed KAPT configuration from module

--- github-stack/db-github/build.gradle.kts
+++ github-stack/db-github/build.gradle.kts
@@ -3,22 +3,9 @@ import ext.coreModule
 plugins {
     id("com.android.library")
     kotlin("android")
-    kotlin("kapt")
-}
-
-android {
-    defaultConfig {
-        kapt {
-            arguments {
-                arg("room.incremental", "true")
-            }
-        }
-    }
 }
 
 dependencies {
-    kapt(Deps.AndroidX.Room.COMPILER)
-
     api(kotlin("stdlib"))
     api(coreModule("domain"))
     api(Deps.Kotlinx.Coroutines.CORE)
