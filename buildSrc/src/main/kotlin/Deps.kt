f39b5235b6c68623dd8580d12660d5528427bcf9
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-20 21:41:36 +0200

    Add plugin refreshVersions
    See documentation at https://jmfayard.github.io/refreshVersions/

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,117 +1,100 @@
 object Deps {
     object AndroidX {
         object Room {
-            private const val VERSION = "2.4.0-alpha02"
-            const val COMPILER = "androidx.room:room-compiler:$VERSION"
-            const val RUNTIME = "androidx.room:room-runtime:$VERSION"
-            const val COMMON = "androidx.room:room-common:$VERSION"
-            const val ROOM_KTX = "androidx.room:room-ktx:$VERSION"
+            const val COMPILER = "androidx.room:room-compiler:_"
+            const val RUNTIME = "androidx.room:room-runtime:_"
+            const val COMMON = "androidx.room:room-common:_"
+            const val ROOM_KTX = "androidx.room:room-ktx:_"
         }
 
         object ConstraintLayout {
-            private const val VERSION = "2.1.0-beta02"
-            const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:$VERSION"
+            const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:_"
         }
 
         object Navigation {
-            private const val VERSION = "2.3.5"
-            const val FRAGMENT = "androidx.navigation:navigation-fragment:$VERSION"
-            const val UI = "androidx.navigation:navigation-ui:$VERSION"
-            const val UI_KTX = "androidx.navigation:navigation-ui-ktx:$VERSION"
-            const val RUNTIME = "androidx.navigation:navigation-runtime:$VERSION"
-            const val RUNTIME_KTX = "androidx.navigation:navigation-runtime-ktx:$VERSION"
+            const val FRAGMENT = "androidx.navigation:navigation-fragment:_"
+            const val UI = "androidx.navigation:navigation-ui:_"
+            const val UI_KTX = "androidx.navigation:navigation-ui-ktx:_"
+            const val RUNTIME = "androidx.navigation:navigation-runtime:_"
+            const val RUNTIME_KTX = "androidx.navigation:navigation-runtime-ktx:_"
         }
 
         object Lifecycle {
-            private const val VERSION = "2.4.0-alpha01"
-            const val COMMON = "androidx.lifecycle:lifecycle-common:$VERSION"
-            const val RUNTIME_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:$VERSION"
-            const val COMMON_JAVA8 = "androidx.lifecycle:lifecycle-common-java8:$VERSION"
-            const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel:$VERSION"
-            const val VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$VERSION"
-            const val LIVEDATA_CORE = "androidx.lifecycle:lifecycle-livedata-core:$VERSION"
-            const val LIVEDATA_CORE_KTX = "androidx.lifecycle:lifecycle-livedata-core-ktx:$VERSION"
-            const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:$VERSION"
+            const val COMMON = "androidx.lifecycle:lifecycle-common:_"
+            const val RUNTIME_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:_"
+            const val COMMON_JAVA8 = "androidx.lifecycle:lifecycle-common-java8:_"
+            const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel:_"
+            const val VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:_"
+            const val LIVEDATA_CORE = "androidx.lifecycle:lifecycle-livedata-core:_"
+            const val LIVEDATA_CORE_KTX = "androidx.lifecycle:lifecycle-livedata-core-ktx:_"
+            const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:_"
         }
 
         object RecyclerView {
-            private const val VERSION = "1.2.0"
-            const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:$VERSION"
+            const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:_"
         }
 
         object Fragment {
-            private const val VERSION = "1.3.3"
-            const val FRAGMENT = "androidx.fragment:fragment:$VERSION"
+            const val FRAGMENT = "androidx.fragment:fragment:_"
         }
 
         object AppCompat {
-            private const val VERSION = "1.3.0-rc01"
-            const val APPCOMPAT = "androidx.appcompat:appcompat:$VERSION"
+            const val APPCOMPAT = "androidx.appcompat:appcompat:_"
         }
 
         object SwipeRefreshLayout {
-            private const val VERSION = "1.2.0-alpha01"
-            const val SWIPE_REFRESH_LAYOUT =
-                "androidx.swiperefreshlayout:swiperefreshlayout:$VERSION"
+            const val SWIPE_REFRESH_LAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:_"
         }
     }
 
     object Google {
         object Material {
-            private const val VERSION = "1.4.0-beta01"
-            const val MATERIAL = "com.google.android.material:material:$VERSION"
+            const val MATERIAL = "com.google.android.material:material:_"
         }
     }
 
     object Kotlinx {
         object Coroutines {
-            private const val VERSION = SharedVersions.Kotlinx.COROUTINES
-            const val CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VERSION"
-            const val ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VERSION"
+            const val CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:_"
+            const val ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:_"
         }
     }
 
     object Square {
         object OkHttp {
-            private const val VERSION = "4.9.1"
-            const val OKHTTP = "com.squareup.okhttp3:okhttp:$VERSION"
-            const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:$VERSION"
+            const val OKHTTP = "com.squareup.okhttp3:okhttp:_"
+            const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:_"
         }
 
         object Retrofit {
-            private const val VERSION = "2.9.0"
-            const val RETROFIT = "com.squareup.retrofit2:retrofit:$VERSION"
+            const val RETROFIT = "com.squareup.retrofit2:retrofit:_"
 
             object Converters {
-                const val MOSHI = "com.squareup.retrofit2:converter-moshi:$VERSION"
+                const val MOSHI = "com.squareup.retrofit2:converter-moshi:_"
             }
         }
 
         object Moshi {
-            private const val VERSION = "1.12.0"
-            const val MOSHI = "com.squareup.moshi:moshi:$VERSION"
-            const val KOTLIN = "com.squareup.moshi:moshi-kotlin:$VERSION"
-            const val KOTLIN_CODEGEN = "com.squareup.moshi:moshi-kotlin-codegen:$VERSION"
-            const val ADAPTERS = "com.squareup.moshi:moshi-adapters:$VERSION"
+            const val MOSHI = "com.squareup.moshi:moshi:_"
+            const val KOTLIN = "com.squareup.moshi:moshi-kotlin:_"
+            const val KOTLIN_CODEGEN = "com.squareup.moshi:moshi-kotlin-codegen:_"
+            const val ADAPTERS = "com.squareup.moshi:moshi-adapters:_"
         }
     }
 
     object Koin {
-        private const val VERSION = "3.0.1"
-        const val CORE = "io.insert-koin:koin-core-jvm:$VERSION"
-        const val ANDROID = "io.insert-koin:koin-android:$VERSION"
+        const val CORE = "io.insert-koin:koin-core-jvm:_"
+        const val ANDROID = "io.insert-koin:koin-android:_"
     }
 
     object Coil {
-        private const val VERSION = "1.2.1"
-        const val COIL = "io.coil-kt:coil:$VERSION"
-        const val COIL_BASE = "io.coil-kt:coil-base:$VERSION"
+        const val COIL = "io.coil-kt:coil:_"
+        const val COIL_BASE = "io.coil-kt:coil-base:_"
     }
 
     object FlowBinding {
-        private const val VERSION = "1.0.0"
-        const val ANDROID = "io.github.reactivecircus.flowbinding:flowbinding-android:$VERSION"
+        const val ANDROID = "io.github.reactivecircus.flowbinding:flowbinding-android:_"
         const val SWIPE_REFRESH_LAYOUT =
-            "io.github.reactivecircus.flowbinding:flowbinding-swiperefreshlayout:$VERSION"
+            "io.github.reactivecircus.flowbinding:flowbinding-swiperefreshlayout:_"
     }
 }
